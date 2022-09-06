9fde546441788a174dfd20548b39e43a16ca1684
Bohdan Mukha <by.muha@gmail.com>
2022-09-06 13:51:43 +0300

    flip on click works

--- js/index.js
+++ js/index.js
@@ -1,1 +1,11 @@
-// alert('it works');
+const cardsWrapper = document.querySelector('.cards-wrapper');
+cardsWrapper.addEventListener('click', (event) => {
+  console.log(event.target.classList);
+  if (event.target.classList.contains('cardback')) {
+    console.log('its card!');
+    event.target.closest('.flipper').classList.add('is-flipped');
+    setTimeout(() => {
+      event.target.closest('.flipper').classList.remove('is-flipped');
+    }, 300);
+  }
+});
